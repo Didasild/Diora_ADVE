ab38d649eddeef6cd05421cbd5f8b6360c22c6cd
Martin <licp-roussel@hotmail.fr>
2021-11-11 16:18:05 +0100

    Asset12

---
+++ Assets/Assets/Animations/Script/AnimationScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour
+{
+    Animator animator;
+
+// Start is called before the first frame update
+void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+            animator.SetTrigger("Extend");
+        if (Input.GetKeyDown(KeyCode.R))
+            animator.SetTrigger("Retract");
+        if (Input.GetKeyDown(KeyCode.Space))
+            animator.SetTrigger("Intro");
+        if (Input.GetKeyDown(KeyCode.Space))
+            animator.SetTrigger("Intro_01");
+       
+    }
+}
---
+++ Assets/Assets/Animations/Script/AnimationTriggerScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTriggerScript : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player");
+        {
+            animator.SetTrigger("Retract");
+            animator.SetTrigger("Extend_Pilar");
+            animator.SetTrigger("Bridge_Open");
+            animator.SetTrigger("Bridge_Open01");
+            animator.SetTrigger("Wall_Trigger");
+        }
+
+    }
+    //private void OnTriggerExit(Collider collider)
+    //{
+    //    if (collider.tag == "Player");
+    //    {
+    //        animator.SetTrigger("Retract");
+    //    }
+    //}
+}
